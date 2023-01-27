9c6072ee86664f82e712c9f0bb0dae1cf93c9e30
Irad Doron <irad.doron@gmail.com>
2023-01-27 10:34:03 +0200

    clean code

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
 import './index.css';
-import reportWebVitals from './reportWebVitals';
 
 import { BrowserRouter } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
@@ -20,4 +19,3 @@ root.render(
 	</React.StrictMode>
 );
 
-reportWebVitals();
